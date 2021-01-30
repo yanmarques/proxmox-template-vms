f08b5541d401554cdc646483e7a76d63839a16ec
Yan Marques <marques_yan@outlook.com>
2021-01-29 22:13:14 -0300

    Improve windows installation script

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,6 +1,17 @@
+# global vars
+$name = "Start templated"
+
 # retrieve service binary path
 # maybe this is easiest and more stable way to do
-$binaryPathName = Join-Path -Path $PSScriptRoot -ChildPath start-templated-vm.ps1
+$psPath = Join-Path -Path $PSScriptRoot -ChildPath start-templated-vm.ps1
+
+# try to get any existing service with our name
+$oldService = Get-WmiObject -Class Win32_Service -Filter "Name='$name'"
+
+# maybe remove it
+if ($oldService -neq $null) {
+    $oldService.Delete()
+}
 
 # register the templated service
-New-Service -Name 'Start templated' -BinaryPathName $binaryPathName
+New-Service -Name $name -BinaryPathName "powershell.exe -File $psPath"
