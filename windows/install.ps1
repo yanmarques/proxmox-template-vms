6cbe3022f61f83e4eb6f2a15e83c22c3b4f797d1
Yan Marques <marques_yan@outlook.com>
2021-01-30 18:31:15 -0300

    Change installation strategy to GPO startup scripts

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,27 +1,61 @@
-# global vars
-$serviceName = "Start templated"
-$scriptFile = "start-templated-vm.ps1"
+param (
+    $script,
+    [String] $userPath='C:\Users\Administrator'
+)
 
-# app directory, it's easier to find our powershell script
-$appDirectory = $PSScriptRoot
+# try to obtain default script from PowerShell variables
+if ($script -eq $null) {
+    $script = Join-Path -Path $PSScriptRoot -ChildPath start-templated-vm.ps1
+}
 
-# escape our powershell path with double quotes
-$appParams = "-ExecutionPolicy Bypass -NoProfile -File $scriptFile"
+function RegisterGPOStartupScript {
+    param (
+        $gpoPath
+    )
 
-# powershell.exe absolute path
-$psWinPath= (Get-Command powershell).Source
+    # how many scripts this path have
+    $existingScriptsCount = (Get-ChildItem -Path $gpoPath).Length
 
-# nssm absolute path
-$nssm = (Get-Command nssm).Source
+    # loop through every possible subkey registry
+    # remove if the script property matches our script
+    for ($count = 0; $count -lt $existingScriptsCount; $count++) {
+        $regPath = Join-Path -Path $gpoPath -ChildPath $count.ToString()
+        $currentScript = (Get-ItemProperty -Path $regPath).Script
+    
+        if ($currentScript -eq $script) {
+            Write-Output ("[+] Removing existing script at index: {0}" -f $count)
+            Remove-Item -Path $regPath
+        }
+    }
 
-# remove any existing service
-& $nssm stop $serviceName confirm
-& $nssm remove $serviceName confirm
+    # the path to our new script
+    $regPath = Join-Path -Path $gpoPath -ChildPath $existingScriptsCount.ToString()
 
-# install service
-& $nssm install $serviceName $psWinPath
+    # the script receives the user path as parameter
+    $parameters = '-UserPath "{0}"' -f $userPath
 
-# set the shiny diamond
-& $nssm set $serviceName AppDirectory $appDirectory
-& $nssm set $serviceName AppParameters $appParams
-& $nssm set $serviceName Start SERVICE_AUTO_START
+    Write-Output ("[+] Creating startup script on: {0}" -f $gpoPath)
+
+    # first create the script registry subkey
+    New-Item -Path $regPath | Out-Null
+
+    # then configure with the required properties
+    New-ItemProperty -Path $regPath -Name ExecTime -Value 0 -PropertyType Qword | Out-Null
+    New-ItemProperty -Path $regPath -Name IsPowershell -Value 1 -PropertyType DWord | Out-Null
+    New-ItemProperty -Path $regPath -Name Parameters -Value $parameters -PropertyType String | Out-Null
+    New-ItemProperty -Path $regPath -Name Script -Value $script -PropertyType String | Out-Null
+
+    Write-Output "[+] Startup script registered"
+}
+
+# don't know why, but group policy needs these two registries filled with our configuration
+RegisterGPOStartupScript "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\Scripts\Startup\0"
+RegisterGPOStartupScript "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine\Scripts\Startup\0"
+
+Write-Output ("[+] Allowing AUTHORITY\SYSTEM user to delete {0}" -f $userPath)
+
+# give AUTHORITY\SYSTEM user the permission to delete the user directory
+$icacls = (Get-Command icacls).Source
+& $icacls $userPath /T /C /grant System:D | Out-Null
+
+Write-Output "[+] Done"
