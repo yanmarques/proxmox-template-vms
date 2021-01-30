2b599ff1a76408eaed86614d44a898ad2b55864e
Yan Marques <marques_yan@outlook.com>
2021-01-30 18:32:26 -0300

    Receive user directory as parameter
    Also add some comments

--- windows/start-templated-vm.ps1
+++ windows/start-templated-vm.ps1
@@ -1,19 +1,13 @@
-# global vars
-$defaultUserPath = "C:\Users\Administrator"
+param (
+    [Parameter(Mandatory)] $userPath
+)
 
 # fetch every disk
 $disks = Get-Disk
 
-# calls nssm to stop our service since we have no clue to continue running
-# it must be called when this script finishes
-function StopService {
-    $nssm = (Get-Command nssm).Source
-    & $nssm stop "Start templated"
-}
-
 # ensure we are on a template-based vm
 if ($disks.Count -lt 2) {
-    StopService
+    write-output "skipping, because we are on the templatevm"
     exit 0
 }
 
@@ -41,7 +35,7 @@ $directory = $winVolume.Name
 
 # calculate some file paths
 $usersDir = Join-Path -Path $directory -ChildPath Users
-$userFromPath = Split-Path -Path $defaultUserPath -Leaf
+$userFromPath = Split-Path -Path $userPath -Leaf
 $theUserDir = Join-Path -Path $usersDir -ChildPath $userFromPath
 
 $configDir = Join-Path -Path $directory -ChildPath Config
@@ -55,7 +49,7 @@ if ($isRaw) {
     # /E - copy directories and subdirectories
     # /C - continue even when something failed
     # /Q - quiet
-    Invoke-Command -ScriptBlock {xcopy $defaultUserPath $theUserDir /I /H /E /C /Q}
+    Invoke-Command -ScriptBlock {xcopy $userPath $theUserDir /I /H /E /C /Q}
 
     # create config directory
     New-Item -Path $configDir -ItemType Directory
@@ -65,14 +59,16 @@ if ($isRaw) {
 }
 
 # maybe remove user directory
-if (Test-Path -Path $defaultUserPath) {
-    Remove-Item -Path $defaultUserPath -Recurse -Force
+if (Test-Path -Path $userPath) {
+    # first remove all subdirectories and files 
+    Get-ChildItem -Path $userPath -Recurse | Remove-Item -Recurse -Force
+    
+    # then remove the actual user directory
+    Remove-Item -Path $userPath -Recurse -Force
 }
 
 # create symbolic link to user directory
-New-Item -Path $defaultUserPath -ItemType SymbolicLink -Value $theUserDir
+New-Item -Path $userPath -ItemType SymbolicLink -Value $theUserDir
 
 # run user startup script
 Invoke-Command -ScriptBlock {powershell $startupFile}
-
-StopService
