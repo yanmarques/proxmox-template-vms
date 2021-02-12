86d071bf0202c5579e03747304f01dcc1b3e93de
Yan Marques de Cerqueira <marques_yan@outlook.com>
2021-02-11 23:21:51 -0300

    improve linux functions tests

--- tests/unix/linux/include.sh
+++ tests/unix/linux/include.sh
@@ -3,30 +3,9 @@
 # shellcheck source=/dev/null
 . ./tests/unix/linux/functions
 
-start() {
-    # artifact to test
-    touch "$skel_dir"/testing-templated
-
-    start_disk "$test_disk"
-
-    umount "$rw_dir"
-
-    # re-mount disk to be sure
-    register_tmp_dir
-    local tmp_dir="$(last_tmp_dir)"
-    loop_mount "$test_disk" "$tmp_dir"
-
-    local expected_dirs=('config' 'binds' 'home')
-    for directory in "${expected_dirs[@]}"; do
-        test -d "$tmp_dir/$directory"
-    done
-
-    test -e "$tmp_dir/$test_home/testing-templated"
-}
-
 with_raw_disk_formated() {
     format_disk_when_raw "$test_disk" "$test_disk" \
-        -F -E offset="$default_part_offset"
+        -F -E offset="$default_part_offset" 2> /dev/null
 }
 
 format_an_already_formated_disk_does_nothing() {
@@ -45,8 +24,4 @@ format_an_already_formated_disk_does_nothing() {
     loop_mount "$test_disk" "$rw_dir"
 
     [ -e "$rw_dir"/templated-test-file ]
-}
-
-checks_dummy_disk_is_raw() {
-    is_raw_disk "$test_disk"
 }
--- tests/unix/linux/test_disks_spec.sh
+++ tests/unix/linux/test_disks_spec.sh
@@ -5,22 +5,69 @@ Include ./tests/unix/linux/include.sh
 BeforeEach "setup"
 AfterEach "teardown"
 
-It "recognizes dummy created disk as raw"
-    When call checks_dummy_disk_is_raw
-    The status should be success
+Describe "disks_counter()"
+    It "1 scsi disk"
+        Mock fdisk
+            echo "Disk /dev/sda"
+        End
+
+        When call disks_counter
+        The output should eq "1"
+    End
+
+    It "3 ide disks"
+        Mock fdisk
+            echo "Disk /dev/hda"
+            echo "Disk /dev/hdb"
+            echo "Disk /dev/hdc"
+        End
+
+        When call disks_counter
+        The output should eq "3"
+    End
+
+    It "1 scsi disk and 1 ide disk"
+        Mock fdisk
+            echo "Disk /dev/sda"
+            echo "Disk /dev/hdc"
+        End
+
+        When call disks_counter
+        The output should eq "2"
+    End
 End
 
-It 'format raw testing disk'
-    When call with_raw_disk_formated
-    The status should be success
-    The stderr should include "Found a dos partition table"
-    The stdout should not include "failed"
-    The variable test_disk should satisfy is_disk
+Describe "format_disk_when_raw()"
+    It 'formats the disk'
+        When call with_raw_disk_formated
+        The status should eq 0
+        The output should not include failed
+        The variable test_disk should satisfy is_disk
+    End
+
+    Context 'when not raw'
+        Before "with_raw_disk_formated"
+
+        It "does not format the disk"
+            When call with_raw_disk_formated
+            The status should eq 1
+        End
+    End
 End
 
-It "format already formated disk keep it's content"
-    When call format_an_already_formated_disk_does_nothing
-    The status should be success
-    The stdout should not include "failed"
-    The stderr should include "Found a dos partition table"
+Describe "is_raw_disk()"
+    It "recognizes dummy created disk as raw"
+        When call is_raw_disk "$test_disk"
+        The status should be success
+    End
+
+    Context 'when disk is not raw'
+        Before "with_raw_disk_formated"
+
+        It "recognizes dummy formated disk as not raw"
+            When call is_raw_disk "$test_disk"
+            The status should be failure
+        End
+    End
 End
+
