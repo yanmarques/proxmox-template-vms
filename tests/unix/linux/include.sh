ccdde95b4e961131649d28af3b769bdd929d0d83
Yan Marques de Cerqueira <marques_yan@outlook.com>
2021-02-11 22:45:57 -0300

    add more disks tests

--- tests/unix/linux/include.sh
+++ tests/unix/linux/include.sh
@@ -28,3 +28,21 @@ with_raw_disk_formated() {
     format_disk_when_raw "$test_disk" "$test_disk" \
         -F -E offset="$default_part_offset"
 }
+
+format_an_already_formated_disk_does_nothing() {
+    with_raw_disk_formated
+    
+    loop_mount "$test_disk" "$rw_dir"
+
+    # artifact to test
+    touch "$rw_dir"/templated-test-file
+
+    umount "$rw_dir"
+
+    # call formatting again
+    with_raw_disk_formated
+
+    loop_mount "$test_disk" "$rw_dir"
+
+    [ -e "$rw_dir"/templated-test-file ]
+}
--- tests/unix/linux/test_disks_spec.sh
+++ tests/unix/linux/test_disks_spec.sh
@@ -7,8 +7,15 @@ AfterEach "teardown"
 
 It 'format raw testing disk'
     When call with_raw_disk_formated
-    The output should not include failed
-    The line 2 of stderr should start with "Found a dos partition table"
+    The status should be success
+    The stderr should include "Found a dos partition table"
+    The stdout should not include "failed"
     The variable test_disk should satisfy is_disk
 End
 
+It "format already formated disk keep it's content"
+    When call format_an_already_formated_disk_does_nothing
+    The status should be success
+    The stdout should not include "failed"
+    The stderr should include "Found a dos partition table"
+End
