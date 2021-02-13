a137e2cf875bc4b2054f0c26cf8a156035026852
Yan Marques de Cerqueira <marques_yan@outlook.com>
2021-02-12 21:35:07 -0300

    let implementation decides how to mount

--- tests/unix/linux/test_disks_spec.sh
+++ tests/unix/linux/test_disks_spec.sh
@@ -125,3 +125,65 @@ Describe "start_disk()" mount_mock
         End
     End
 End
+
+Describe "mount_strategy()"
+    
+    It "mounts disks when source is a block device"
+        mount() {
+            # shellcheck disable=SC2154
+            [ "$1" == "$test_disk" ] && [ "$2" == "$rw_dir" ]
+        }
+
+        # shellcheck disable=SC2154
+        When call mount_strategy "$test_disk" "$rw_dir" --force-disk
+        The status should be success
+    End
+
+    It "bind mounts when source is not a block device"
+        mount() {
+            [ "$1" == --bind ]
+        }
+
+        # shellcheck disable=SC2154
+        When call mount_strategy "$test_disk" "$rw_dir"
+        The status should be success
+    End
+
+    It "creates mountpoint file when missing"
+        mount() {
+            :
+        }
+
+        # shellcheck disable=SC2154
+        When call mount_strategy "$test_disk" "$rw_dir/testing/dummy"
+
+        The value "$rw_dir/testing/dummy" should be a file
+    End
+
+    It "creates mountpoint directory when missing"
+        mount() {
+            :
+        }
+
+        # shellcheck disable=SC2154
+        When call mount_strategy "$rw_dir" "$rw_dir/testing/dummy"
+
+        The value "$rw_dir/testing/dummy" should be a directory
+    End
+
+    Context
+        cleanup() {
+            umount "$rw_dir"/*
+        }
+        
+        After "cleanup"
+
+        It "creates mountpoint from file"
+            # shellcheck disable=SC2154
+            When call mount_strategy "$test_disk" "$rw_dir/dummy"
+
+            Assert same_contents "$test_disk" "$rw_dir/dummy"
+        End
+    End
+
+End
