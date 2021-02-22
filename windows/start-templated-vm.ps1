7ca331a46634dbb0f96bdf10fe9de227f0239ff3
Yan Marques de Cerqueira <marques_yan@outlook.com>
2021-02-22 16:21:09 -0300

    maybe fix no logging

--- windows/start-templated-vm.ps1
+++ windows/start-templated-vm.ps1
@@ -6,7 +6,9 @@ param (
 $OutFile = "C:\Temp\templated\out.log"
 $ErrFile = "C:\Temp\templated\err.log"
 
-function PathName-Of {
+Write-Output "pre-starting templated-vm" > $OutFile
+
+function Get-FullPath {
     param (
         [String] $File
     )
@@ -14,14 +16,12 @@ function PathName-Of {
     Join-Path -Path "C:\Program Files\proxmox-template-vms\windows" -ChildPath $File
 }
 
-Write-Output "pre-starting templated-vm" > $OutFile
-
-$AuthModule = PathName-Of "Auth.psm1"
+$AuthModule = Get-FullPath "Auth.psm1"
 Write-Output "importing module: $AuthModule" >> $OutFile
 Import-Module $AuthModule
 
 Write-Output "starting templated-vm" >> $OutFile
 
-$Functions = PathName-Of "Functions.psm1"
+$Functions = Get-FullPath "Functions.psm1"
 $Arguments = "-Command `"Import-Module '{0}' ; Start-Templated -UserPath '{1}'`"" -f $Functions,$UserPath
 Start-ElevatedPS -Arguments $Arguments -OutFile $OutFile -ErrFile $ErrFile
