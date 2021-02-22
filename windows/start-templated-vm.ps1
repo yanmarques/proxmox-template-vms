3ab72425ddddd32a902292ebe4be0a011ae12143
Yan Marques de Cerqueira <marques_yan@outlook.com>
2021-02-22 14:12:46 -0300

    add logging

--- windows/start-templated-vm.ps1
+++ windows/start-templated-vm.ps1
@@ -16,6 +16,8 @@ Import-Module $AuthModule
 $OutFile = "C:\Temp\templated.out.log"
 $ErrFile = "C:\Temp\templated.err.log"
 
+Write-Output "starting templated-vm" > $OutFile
+
 $Functions = PathName-Of "Functions.psm1"
 $Arguments = "-Command `"Import-Module '{0}' ; Start-Templated -UserPath '{1}'`"" -f $Functions,$UserPath
 Start-ElevatedPS -Arguments $Arguments -OutFile $OutFile -ErrFile $ErrFile
