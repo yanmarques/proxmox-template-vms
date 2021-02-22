7ef319f61938016f50d8333916dbc55911ba1856
Yan Marques de Cerqueira <marques_yan@outlook.com>
2021-02-22 17:10:59 -0300

    improve script logging

--- windows/start-templated-vm.ps1
+++ windows/start-templated-vm.ps1
@@ -3,10 +3,14 @@ param (
 )
 
 # Default logging targets
+$LogFile = "C:\Temp\templated\log"
 $OutFile = "C:\Temp\templated\out.log"
 $ErrFile = "C:\Temp\templated\err.log"
 
-Write-Output "pre-starting templated-vm" > $OutFile
+# ensure directory exists
+New-Item -Path "C:\Temp\templated\" -ItemType Directory -Force
+
+Write-Output "pre-starting templated-vm" > $LogFile
 
 function Get-FullPath {
     param (
@@ -17,11 +21,18 @@ function Get-FullPath {
 }
 
 $AuthModule = Get-FullPath "Auth.psm1"
-Write-Output "importing module: $AuthModule" >> $OutFile
+Write-Output "importing module: $AuthModule" >> $LogFile
 Import-Module $AuthModule
 
-Write-Output "starting templated-vm" >> $OutFile
+Write-Output "starting templated-vm" >> $LogFile
 
 $Functions = Get-FullPath "Functions.psm1"
 $Arguments = "-Command `"Import-Module '{0}' ; Start-Templated -UserPath '{1}'`"" -f $Functions,$UserPath
+
+$Now = Get-Date -UFormat "%d-%m-%Y %H:%M:%S"
+Write-Output "starting process at: $Now" >> $LogFile
+
 Start-ElevatedPS -Arguments $Arguments -OutFile $OutFile -ErrFile $ErrFile
+
+$Now = Get-Date -UFormat "%d-%m-%Y %H:%M:%S"
+Write-Output "ending process at: $Now" >> $LogFile
