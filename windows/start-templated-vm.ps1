e2227c6f234cc9d52a90d54e66dd09cbe5b68e99
Yan Marques de Cerqueira <marques_yan@outlook.com>
2021-02-22 12:48:27 -0300

    force the use of double quotes on command argument

--- windows/start-templated-vm.ps1
+++ windows/start-templated-vm.ps1
@@ -17,5 +17,5 @@ $OutFile = "C:\Temp\templated.out.log"
 $ErrFile = "C:\Temp\templated.err.log"
 
 $Functions = PathName-Of "Functions.psm1"
-$Arguments = "-Command "Import-Module '{0}' ; Start-Templated -UserPath '{1}'"" -f $Functions,$UserPath
+$Arguments = "-Command `"Import-Module '{0}' ; Start-Templated -UserPath '{1}'`"" -f $Functions,$UserPath
 Start-ElevatedPS -Arguments $Arguments -OutFile $OutFile -ErrFile $ErrFile
